3dde5f0666c574365ed00dd39572a361d22c1184
Everton Alves <alveseverton023@gmail.com>
2022-08-23 05:54:51 -0300

    Criação formulario 2 parte1

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
-let formulario = document.createElement("form");
+let formulario = document.createElement('form');
 let titulo = document.createElement('input');
-let descricao = document.createElement("input");
-let link = document.createElement("input");
-let botao = document.createElement("button");
+let descricao = document.createElement('input');
+let link = document.createElement('input');
+let botao = document.createElement('button');
+let sabre = document.createElement('input');
 
 document.body.appendChild(formulario)
 
@@ -17,12 +18,30 @@ document.getElementById('desc');
 formulario.appendChild(link);
 link.id = 'lk';
 document.getElementById('lk');
+link.setAttribute('src');
+
+formulario.appendChild(sabre);
+sabre.id = 'sb';
+document.getElementById('sb');
 
 formulario.appendChild(botao);
 botao.id = 'but';
 document.getElementById('but');
 
-botao.addEventListener();
+function criarform2(){
+    let card = document.createElement('div');
+    let tituloCard = document.getElementsById('titulof');
+    let imagem = document.getElementsById('lk');
+    let descricaoCard = document.getElementsById('desc');
+
+    document.body.appendChild(card);
+    card.id = 'card1';
+    document.card.appendChild(tituloCard);
+    document.card.appendChild(imagem);
+    document.card.appendChild(descricaoCard);
+}
+
+botao.addEventListener(criarform2);
 
 formulario.id = 'forms';
 document.getElementById('forms');
