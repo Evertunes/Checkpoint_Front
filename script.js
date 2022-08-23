35a5d4fb8e93b5c1e3800d62861f7c9a2cdc24f7
Everton Alves <alveseverton023@gmail.com>
2022-08-22 22:38:39 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,18 @@
+let formulario = document.createElement("form");
+let titulo = document.createElement('input');
+let descricao = document.createElement("input");
+let link = document.createElement("input");
+let botao = document.createElement("button");
+
+document.body.appendChild(formulario)
+
+formulario.appendChild(titulo);
+titulo.id = 'titulof';
+document.getElementById('titulof');
+
+formulario.appendChild(descricao);
+descricao.id = 'desc';
+document.getElementById('desc');
+
+formulario.id = 'forms';
+document.getElementById('forms');
