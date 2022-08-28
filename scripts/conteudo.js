dc9544d097620f3999740dfb14ed244441aba3cc
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-08-27 23:28:09 -0300

    botao gera cads automaticamente pronto

--- scripts/conteudo.js
+++ scripts/conteudo.js
@@ -1,94 +1,60 @@
-const infoPersonagens = [
+const info = [
   {
     nome: "Darth Vader",
-    descric:
+    descricao:
       "Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
     afiliacao: "ordem sith, império galáctico",
-    cordosabre: "vermelho",
-    imgC: "https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg",
+    corDoSabre: "vermelho",
+    image:
+      "https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg",
   },
 
   {
     nome: "Luke Skywalker",
-    descric:
+    descricao:
       "Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
     afiliacao: "ordem jedi",
-    cordosabre: "verde",
-    imgC: "https://wallpapercave.com/wp/wp3800518.jpg",
+    corDoSabre: "verde",
+    image: "https://wallpapercave.com/wp/wp3800518.jpg",
   },
 
   {
     nome: "Darth Maul",
-    descric:
+    descricao:
       "Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
     afiliacao: "ordem sith",
-    cordosabre: "vermelho",
-    imgC: "https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg",
+    corDoSabre: "vermelho",
+    image:
+      "https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg",
   },
 
   {
     nome: "Obi-Wan Kenobi",
-    descric:
+    descricao:
       "Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
     afiliacao: "ordem jedi, república galáctica",
-    cordosabre: "azul",
-    imgC: "https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg",
+    corDoSabre: "azul",
+    image:
+      "https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg",
   },
 
   {
     nome: "Darth Sirius",
-    descric:
+    descricao:
       "Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
     afiliacao: "ordem sith, império galáctico",
-    cordosabre: "vermelho",
-    imgC: "https://images.saymedia-content.com/.image/t_share/MTc1MTEwOTE1NTg5NjEyNjM5/five-unsolved-star-wars-mysteries.jpg",
+    corDoSabre: "vermelho",
+    image:
+      "https://images.saymedia-content.com/.image/t_share/MTc1MTEwOTE1NTg5NjEyNjM5/five-unsolved-star-wars-mysteries.jpg",
   },
 
   {
     nome: "Mestre Yoda",
-    descric:
+    descricao:
       "Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
     afiliacao: "ordem jedi, república galáctica",
-    cordosabre: "verde",
-    imgC: "https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+    corDoSabre: "verde",
+    image:
+      "https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
   },
 ];
-
-/* 
-    
-    var containerPai = document.createElement('section');
-    containerPai.id = "cardsp"
-    var nomeCard = document.createElement('p');
-    var descricaoCard = document.createElement('p');
-    var afiliacaoCard = document.createElement('p');
-    var cordosabreCard = document.createElement('p');
-    var imagemCard = document.createElement('img');
-    
-function criaCard(){
-    
-        for(const criar of info){
-           
-           
-            containerPai.appendChild(nomeCard,descricaoCard, afiliacaoCard, cordosabreCard,imagemCard)
-            document.body.appendChild(containerPai);
-
-        }
-    };
-
-
-    
-function preencher(){
-        for(const dadosConteudo of info){
-            let tit = document.querySelector("nomeCard")
-            tit.setAttribute("nome")
-            let desc = document.querySelector("descricaoCard")
-            desc.setAttribute("descic")
-            let af = document.querySelector("afiliacaoCard")
-            af.setAttribute("afiliacao")
-            let cors = document.querySelector("cordosabreCard")
-            cors.setAttribute("cordosabre")
-            let im = document.querySelector("imagemCard")
-            im.setAttribute("imgC")
-        }
-    }
- */
--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,9 @@ const inputAfiliacaoPersonagem = document.getElementById(
 const personagemContainercard = document.getElementById(
   "personagem-containercard"
 );
+const botaoPreencher = document.getElementById("botao-preencher");
 
-// eventos
+// cria os cards com as informações do formulário
 formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
   let dadosPersonagem = `
@@ -33,19 +34,82 @@ formulario.addEventListener("submit", (evento) => {
   inputAfiliacaoPersonagem.value = null;
 });
 
-/* 
+// cria os cards automaticamente com as informações do array
 botaoPreencher.addEventListener("click", (evento) => {
-    
-    evento.preventDefault()
-    
-        console.log("teste");
-    
-        
-        let tituloCard = `
-        <img src="${link.value}"/>
-        <span>"${titulo.value}"</span>
-        <span>"${descricao.value}"</span>
-        <span>"${sabre.value}"</span>
-        `
-        document.getElementById("cards").innerHTML += tituloCard;
-    }); */
+  evento.preventDefault();
+  console.log(infoPersonagens[1].nome);
+  infoPersonagens.forEach((number) => {
+    let dadosPersonagem = `
+    <div class="card">
+      <h2 class="card-nome">${number.nome}</h2>
+      <img class="card-imagem" src="${number.image}" alt="imagem do personagem"/>
+      <p class="card-descricao">${number.descricao}</p>
+      <h3 class="card-cor-sabre">Utiliza um sabre ${number.corDoSabre}</h3>
+      <h3 class="card-afiliacao">Pertence a ${number.afiliacao}</h3>
+      </div>
+      `;
+    personagemContainercard.innerHTML += dadosPersonagem;
+  });
+});
+
+// array
+const infoPersonagens = [
+  {
+    nome: "Darth Vader",
+    descricao:
+      "Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
+    afiliacao: "ordem sith, império galáctico",
+    corDoSabre: "vermelho",
+    image:
+      "https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg",
+  },
+
+  {
+    nome: "Luke Skywalker",
+    descricao:
+      "Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
+    afiliacao: "ordem jedi",
+    corDoSabre: "verde",
+    image: "https://wallpapercave.com/wp/wp3800518.jpg",
+  },
+
+  {
+    nome: "Darth Maul",
+    descricao:
+      "Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
+    afiliacao: "ordem sith",
+    corDoSabre: "vermelho",
+    image:
+      "https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg",
+  },
+
+  {
+    nome: "Obi-Wan Kenobi",
+    descricao:
+      "Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
+    afiliacao: "ordem jedi, república galáctica",
+    corDoSabre: "azul",
+    image:
+      "https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg",
+  },
+
+  {
+    nome: "Darth Sirius",
+    descricao:
+      "Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
+    afiliacao: "ordem sith, império galáctico",
+    corDoSabre: "vermelho",
+    image:
+      "https://images.saymedia-content.com/.image/t_share/MTc1MTEwOTE1NTg5NjEyNjM5/five-unsolved-star-wars-mysteries.jpg",
+  },
+
+  {
+    nome: "Mestre Yoda",
+    descricao:
+      "Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
+    afiliacao: "ordem jedi, república galáctica",
+    corDoSabre: "verde",
+    image:
+      "https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+  },
+];
