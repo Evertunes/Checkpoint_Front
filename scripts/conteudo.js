f17cb892b925385f18576284e4b743d23fbf3a4c
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-08-27 21:30:10 -0300

    Ajustes na estrutura dos cads

--- scripts/conteudo.js
+++ scripts/conteudo.js
@@ -1,52 +1,60 @@
-const info = [
-    {
-        nome: 'Darth Vader',
-        descric: 'Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio',
-        afiliacao: 'darth vader -- ordem sith, império galáctico',
-        cordosabre: 'darth vader -- vermelho',
-        imgC: 'https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg'
-    },
-    
-    {
-        nome: 'Luke Skywalker',
-        descric: 'Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes',
-        afiliacao: 'luke skywalker -- ordem jedi',
-        cordosabre: 'luke skywalker -- verde',
-        imgC: 'https://wallpapercave.com/wp/wp3800518.jpg'
-    },
-    
-    {
-        nome: 'Darth Maul',
-        descric: 'Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição',
-        afiliacao: 'darth maul -- ordem sith',
-        cordosabre: 'darth maul --  vermelho',
-        imgC: 'https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg'
-    },
-    
-    {
-        nome: 'Obi-Wan Kenobi',
-        descric: 'Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo',
-        afiliacao: 'obi-wan kenobi -- ordem jedi, república galáctica',
-        cordosabre: 'obi-wan kenobi -- azul',
-        imgC: 'https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg'
-    },
-    
-    {
-        nome: 'Darth Sirius',
-        descric: 'Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência',
-        afiliacao: 'darth sidius -- ordem sith, império galáctico',
-        cordosabre: 'darth sidius -- vermelho',
-        imgC: 'https://images.saymedia-content.com/.image/t_share/MTc1MTEwOTE1NTg5NjEyNjM5/five-unsolved-star-wars-mysteries.jpg'
-    },
-    
-    {
-        nome: 'Mestre Yoda',
-        descric: 'Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.',
-        afiliacao: 'mestre yoda -- ordem jedi, república galáctica',
-        cordosabre: 'mestre yoda -- verde',
-        imgC: 'https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89'
-    }, 
-    ];
+const infoPersonagens = [
+  {
+    nome: "Darth Vader",
+    descric:
+      "Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
+    afiliacao: "ordem sith, império galáctico",
+    cordosabre: "vermelho",
+    imgC: "https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg",
+  },
+
+  {
+    nome: "Luke Skywalker",
+    descric:
+      "Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
+    afiliacao: "ordem jedi",
+    cordosabre: "verde",
+    imgC: "https://wallpapercave.com/wp/wp3800518.jpg",
+  },
+
+  {
+    nome: "Darth Maul",
+    descric:
+      "Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
+    afiliacao: "ordem sith",
+    cordosabre: "vermelho",
+    imgC: "https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg",
+  },
+
+  {
+    nome: "Obi-Wan Kenobi",
+    descric:
+      "Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
+    afiliacao: "ordem jedi, república galáctica",
+    cordosabre: "azul",
+    imgC: "https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg",
+  },
+
+  {
+    nome: "Darth Sirius",
+    descric:
+      "Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
+    afiliacao: "ordem sith, império galáctico",
+    cordosabre: "vermelho",
+    imgC: "https://images.saymedia-content.com/.image/t_share/MTc1MTEwOTE1NTg5NjEyNjM5/five-unsolved-star-wars-mysteries.jpg",
+  },
+
+  {
+    nome: "Mestre Yoda",
+    descric:
+      "Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
+    afiliacao: "ordem jedi, república galáctica",
+    cordosabre: "verde",
+    imgC: "https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+  },
+];
+
+/* 
     
     var containerPai = document.createElement('section');
     var nomeCard = document.createElement('h2');
@@ -78,3 +86,4 @@ const info = [
             im.setAttribute("imgC")
         }
     }
+ */
--- scripts/script.js
+++ scripts/script.js
@@ -1,90 +1,39 @@
-let formulario = document.createElement('form');
-formulario.classList.toggle('formulario');
-let titulo = document.createElement('input');
-titulo.classList.toggle('titulo');
-let descricao = document.createElement('textarea');
-descricao.classList.toggle('descricao');
-let link = document.createElement('input');
-link.classList.toggle('link');
-let sabre = document.createElement('input');
-sabre.classList.toggle('sabre');
-formulario.appendChild(sabre);
-let botao = document.createElement('button');
-botao.classList.toggle('botao');
+// variáveis
+const formulario = document.getElementById("formulario");
+const inputNomePersonagem = document.getElementById("input-nome-personagem");
+const inputUrlImagem = document.getElementById("input-url-imagem");
+const inputDescricaoPersonagem = document.getElementById(
+  "input-descricao-personagem"
+);
+const inputCorSabre = document.getElementById("input-cor-sabre");
+const inputAfiliacaoPersonagem = document.getElementById(
+  "input-afiliacao-personagem"
+);
+const personagemContainercard = document.getElementById(
+  "personagem-containercard"
+);
 
-let botaoPreencher = document.createElement('button');
-botaoPreencher.classList.toggle('botao');
-
-let lbl = document.createTextNode("Enviar Dados");
-botao.appendChild(lbl);
-
-document.body.appendChild(formulario);
-
-formulario.id = 'forms';
-document.getElementById('forms');
-
-let lbltitulo = document.createTextNode("Digite o nome do personagem: ");
-formulario.appendChild(lbltitulo);
-formulario.appendChild(titulo);
-
-titulo.id = 'titulof';
-titulo.setAttribute("required",true)
-document.getElementById('titulof');
-
-let lbltituloDesc = document.createTextNode("Digite a descrição do personagem: ");
-formulario.appendChild(lbltituloDesc);
-formulario.appendChild(descricao);
-descricao.id = 'desc';
-descricao.setAttribute("required",true);
-
-document.getElementById('desc');
-
-let lbltituloLink = document.createTextNode("Insira a URL da imagem: ");
-formulario.appendChild(lbltituloLink);
-formulario.appendChild(link);
-link.id = 'lk';
-link.setAttribute("required",true);
-document.getElementById('lk');
-
-let lbltituloSabre = document.createTextNode("Cor do Sabre: ");
-formulario.appendChild(lbltituloSabre);
-formulario.appendChild(sabre);
-sabre.id = 'sb';
-sabre.setAttribute("required",true);
-document.getElementById('sb');
-
-formulario.appendChild(botao);
-botao.id = 'but';
-document.getElementById('but');
-
-
+// eventos
 formulario.addEventListener("submit", (evento) => {
-    
-evento.preventDefault()
-
-    console.log("formulario enviado");
-
-    let tituloCard = `
-    <img src="${link.value}"/>
-    <span>"${titulo.value}"</span>
-    <span>"${descricao.value}"</span>
-    <span>"${sabre.value}"</span>
-    `
-    document.getElementById("cards").innerHTML += tituloCard;
-    link.value = null
-    titulo.value = null
-    descricao.value = null
-    sabre.value = null
+  evento.preventDefault();
+  let dadosPersonagem = `
+  <div class="card">
+    <h2 class="card-nome">${inputNomePersonagem.value}</h2>
+    <img class="card-imagem" src="${inputUrlImagem.value}" alt="imagem do personagem"/>
+    <p class="card-descricao">${inputDescricaoPersonagem.value}</p>
+    <h3 class="card-cor-sabre">Utiliza um sabre ${inputCorSabre.value}</h3>
+    <h3 class="card-afiliacao">Pertence a ${inputAfiliacaoPersonagem.value}</h3>
+    </div>
+    `;
+  personagemContainercard.innerHTML += dadosPersonagem;
+  inputNomePersonagem.value = null;
+  inputUrlImagem.value = null;
+  inputDescricaoPersonagem.value = null;
+  inputCorSabre.value = null;
+  inputAfiliacaoPersonagem.value = null;
 });
 
-botaoPreencher.id = 'botaoPreencher';
-document.getElementById('botaoPreencher');
-let botaoPreencherDados = document.createTextNode("Preencher Automaticamente");
-botaoPreencher.appendChild(botaoPreencherDados);
-
-let bloco = document.createElement('section');
-formulario.appendChild(botaoPreencher);
-
+/* 
 botaoPreencher.addEventListener("click", (evento) => {
     
     evento.preventDefault()
@@ -99,9 +48,4 @@ botaoPreencher.addEventListener("click", (evento) => {
         <span>"${sabre.value}"</span>
         `
         document.getElementById("cards").innerHTML += tituloCard;
-    });
-
-    
-let card = document.createElement('section');
-card.id = 'cards'
-document.body.appendChild(card);
+    }); */
