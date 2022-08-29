d12082dc61e208d69a3bac69ec224606062ab523
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-08-29 17:28:22 -0300

    ajsutes no layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 // variáveis
+const principal = document.querySelector("body")
 const formulario = document.getElementById("formulario");
 const inputNomePersonagem = document.getElementById("input-nome-personagem");
 const inputUrlImagem = document.getElementById("input-url-imagem");
@@ -45,6 +46,7 @@ else {
   inputDescricaoPersonagem.value = null;
   inputCorSabre.value = "";
   inputAfiliacaoPersonagem.value = null;
+  personagemContainercard.scrollIntoView()
 });
 
 // cria os cards automaticamente com as informações do array
@@ -68,6 +70,7 @@ alert(`Cards automáticos já gerados!
 Clique em "Limpar Cards" ou recarregue a página.
 `)
 }
+personagemContainercard.scrollIntoView()
 });
 
 // limpa os cards gerados
@@ -77,6 +80,7 @@ botaoLimpar.addEventListener("click", () => {
   }
   else{
   personagemContainercard.innerHTML = '';}
+  principal.scrollIntoView()
 });
 
 // array utilizados nos cards gerados automaticamente
@@ -84,7 +88,7 @@ const infoPersonagens = [
   {
     nome: "Darth Vader",
     descricao:
-      "- Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
+      "Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
     afiliacao: "ordem sith, império galáctico",
     corDoSabre: "vermelho",
     image:
@@ -94,7 +98,7 @@ const infoPersonagens = [
   {
     nome: "Luke Skywalker",
     descricao:
-      "- Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
+      "Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
     afiliacao: "ordem jedi",
     corDoSabre: "verde",
     image: "https://wallpapercave.com/wp/wp3800518.jpg",
@@ -103,7 +107,7 @@ const infoPersonagens = [
   {
     nome: "Darth Maul",
     descricao:
-      "- Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
+      "Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
     afiliacao: "ordem sith",
     corDoSabre: "vermelho",
     image:
@@ -113,7 +117,7 @@ const infoPersonagens = [
   {
     nome: "Obi-Wan Kenobi",
     descricao:
-      "- Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
+      "Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
     afiliacao: "ordem jedi, república galáctica",
     corDoSabre: "azul",
     image:
@@ -123,7 +127,7 @@ const infoPersonagens = [
   {
     nome: "Darth Sidious",
     descricao:
-      "- Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
+      "Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
     afiliacao: "ordem sith, império galáctico",
     corDoSabre: "vermelho",
     image:
@@ -133,7 +137,7 @@ const infoPersonagens = [
   {
     nome: "Mestre Yoda",
     descricao:
-      "- Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
+      "Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
     afiliacao: "ordem jedi, república galáctica",
     corDoSabre: "verde",
     image:
