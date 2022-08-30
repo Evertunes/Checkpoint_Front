776022110a9fd7085823d144e77cc972061dd891
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-08-29 23:15:46 -0300

    removido formulario dark

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,10 +1,5 @@
-const change = document.getElementById('ch')
-const inputDark = document.querySelectorAll('input')
-const inputDark2 = document.getElementsByClassName('form-control-lg')
+const change = document.getElementById("ch");
 
-ch.addEventListener('change', () => {
- document.body.classList.toggle('dark')
- inputDark.forEach((array) => {
- array.classList.toggle('bg-dark')
- });
-})
+ch.addEventListener("change", () => {
+ document.body.classList.toggle("dark");
+});
