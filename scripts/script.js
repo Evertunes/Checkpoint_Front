793362c70eac2465834a654e218bea1275f53593
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-08-30 20:51:13 -0300

    ajuste na importaçao de variavel e no botaotop

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+// importações de variáveis
+import infoPersonagens from "./conteudo.js";
+
 // variáveis
 const principal = document.querySelector("body");
 const formulario = document.getElementById("formulario");
@@ -15,6 +18,7 @@ const personagemContainercard = document.getElementById(
 );
 const botaoPreencher = document.getElementById("botao-preencher");
 const botaoLimpar = document.getElementById("botao-limpar");
+const irParaTopo = document.getElementById("top");
 
 // cria os cards com as informações do formulário
 formulario.addEventListener("submit", (evento) => {
@@ -49,7 +53,7 @@ formulario.addEventListener("submit", (evento) => {
   personagemContainercard.scrollIntoView();
 });
 
-// cria os cards automaticamente com as informações do array
+// cria cards automaticamente com as informações do array
 botaoPreencher.addEventListener("click", () => {
   if (personagemContainercard.querySelector(".card-auto") === null) {
     infoPersonagens.forEach((array) => {
@@ -72,7 +76,7 @@ Clique em "Limpar Cards" ou recarregue a página.
   personagemContainercard.scrollIntoView();
 });
 
-// limpa os cards gerados
+// limpa todos cards gerados
 botaoLimpar.addEventListener("click", () => {
   if (personagemContainercard.querySelector(".card") === null) {
     alert(`Não há nenhum card gerado.`);
@@ -82,64 +86,12 @@ botaoLimpar.addEventListener("click", () => {
   principal.scrollIntoView();
 });
 
-// array utilizados nos cards gerados automaticamente
-const infoPersonagens = [
-  {
-    nome: "Darth Vader",
-    descricao:
-      "Darth Vader (anteriormente chamado Anakin Skywalker) é um personagem  da saga de ficção científica Star Wars ; um dos protagonistas centrais da trama que começa no lado da luz mas é corrompido pro lado sombrio",
-    afiliacao: "ordem sith, império galáctico",
-    corDoSabre: "vermelho",
-    image:
-      "https://www.hdwallpaperslife.com/wp-content/uploads/2019/04/new-darth-vader-3z-1680x1050.jpg",
-  },
+// subir para o topo ao clicar
+irParaTopo.addEventListener("click", () => {
+  principal.scrollIntoView();
+});
 
-  {
-    nome: "Luke Skywalker",
-    descricao:
-      "Luke Skywalker é o protagonista da trilogia original da série Star Wars, Um personagem essencial e importante na luta da Aliança Rebelde contra o Império Galáctico sendo um dos últimos jedis existentes",
-    afiliacao: "ordem jedi",
-    corDoSabre: "verde",
-    image: "https://wallpapercave.com/wp/wp3800518.jpg",
-  },
-
-  {
-    nome: "Darth Maul",
-    descricao:
-      "Darth Maul é um Zabrak do planeta Dathomir no universo fictício de Star Wars. Os chifres são a marca registrada de todos os Zabraks, com a face tatuada em vermelho e preto sua aparência é maléfica, ele possui o medo como aliado, Implacável e próximo da perfeição",
-    afiliacao: "ordem sith",
-    corDoSabre: "vermelho",
-    image:
-      "https://wallpapertag.com/wallpaper/full/1/2/0/374617-darth-maul-wallpaper-2481x1520-for-ipad.jpg",
-  },
-
-  {
-    nome: "Obi-Wan Kenobi",
-    descricao:
-      "Obi-Wan Kenobi é um personagem fictício da saga de ficção científica Star Wars, que participou dos seis primeiros filmes, é um dos poucos personagens da série a aparecer nos seis primeiros episódios, dada a sua importância no enredo",
-    afiliacao: "ordem jedi, república galáctica",
-    corDoSabre: "azul",
-    image:
-      "https://superherobrasil.com.br/wp-content/uploads/2019/08/Obi-Wan-Kenobi.jpg",
-  },
-
-  {
-    nome: "Darth Sidious",
-    descricao:
-      "Darth Sidious é o principal antagonista da franquia Star Wars (mais especificamente da Saga Skywalker), tendo sido o principal vilão na trilogia prequel, um dos dois principais junto com Darth Vader na trilogia original e um dos dois principais, juntamente com Kylo Ren, na trilogia de sequência",
-    afiliacao: "ordem sith, império galáctico",
-    corDoSabre: "vermelho",
-    image:
-      "https://www.tvgroove.com/wp-content/uploads/2020/07/darthsidius.jpg",
-  },
-
-  {
-    nome: "Mestre Yoda",
-    descricao:
-      "Yoda é um membro de uma espécie de nome desconhecido. Ele foi o Grão Mestre da Ordem Jedi durante as Guerras Clônicas, um dos membros mais importantes do Alto Conselho Jedi nos últimos dias da República Galáctica, e um dos mais sábios e poderosos Jedi de todos os tempos.",
-    afiliacao: "ordem jedi, república galáctica",
-    corDoSabre: "verde",
-    image:
-      "https://townsquare.media/site/442/files/2012/04/Yoda.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
-  },
-];
+// mudar cursor para tipo 'pointer'
+irParaTopo.addEventListener("mouseover", () => {
+  irParaTopo.style.cursor = 'pointer';
+});
